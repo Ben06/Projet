cdf90c287252b6940f18a9ce6c160f906b0ed07e
Ben06 <compteplaync@gmail.com>
2014-11-11 16:28:40 +0100

    Liste cliquable, navigation dans l'arborescence
    Liste du contenu des dossiers changé dynamiquement
    Possibilité de remonter dans l'arborescence via le bouton "up" (la liste
    du contenu sera changé automatiquement)
    Possibilité de double cliquer sur un élément de la liste pour en voir
    son contenu (liste du contenu changée automatiquement)
    Bouton "home" permettant de revenir au repertoire initiale (à
    généraliser par la suite)
    
    Bugs : NullPointerException lorsque l'on essaye de lister le contenu
    d'un dossier avec des espaces ou dans les dossiers cachés.

--- src/fr/miage/fileListing/FileListing.java
+++ src/fr/miage/fileListing/FileListing.java
@@ -8,102 +8,101 @@ import java.util.List;
 // bug : lorsque l'on remonte jusqu'a c:, ou que l'on souhaite définir c: répertoire courant, on ne peut pas, à la place le repertoire courant
 // "." est ajouté à la place. A corriger par la suite.
 
-
 public class FileListing {
 
 	/**
 	 * liste du contenu du repertoire que l'on souhaite afficher
 	 */
 	List<File> contenu = new ArrayList<>();
-	
+
+	List<String> fileNames = new ArrayList<>();
 	/**
 	 * repertoire courant (celui selectionné par l'utilisateur, "." par default
 	 */
 	File repCourant;
-	
+
 	// au démarrage, afficher le contenu du dossier courant (default)
-	FileListing()
-	{
+	FileListing() {
 		repCourant = new File(".");
-//		System.out.println("FileListing.FileListing() "+repCourant.getCanonicalPath());
-//		System.out.println(repCourant.getPath());
-		File[] files = repCourant.listFiles();
-		for (int i = 0; i<files.length; i++)
-		{
-//			System.out.println(files[i].getCanonicalPath());
-			contenu.add(files[i]);
+		File[] listFiles = repCourant.listFiles();
+		for (int i = 0; i < listFiles.length; i++) {
+			contenu.add(listFiles[i]);
+			fileNames.add(listFiles[i].getName());
 		}
 	}
-	
-	
-	/////////////////////////////////////////////////////////////////////
-	//////////////////////////// Getters & Setters //////////////////////
-	/////////////////////////////////////////////////////////////////////
+
+	// ///////////////////////////////////////////////////////////////////
+	// ////////////////////////// Getters & Setters //////////////////////
+	// ///////////////////////////////////////////////////////////////////
 
 	public File getRepCourant() {
 		return this.repCourant;
 	}
 
+	public List<String> getFileNames() {
+		return fileNames;
+	}
+
+	public void setFileNames(List<String> fileNames) {
+		this.fileNames = fileNames;
+	}
+
 	public void setRepCourant(String repCourant) {
+		System.out.println("FileListing.setRepCourant() repToChange : "+repCourant);
 		File repToChange = new File(repCourant);
-		if (repToChange.isDirectory())
-		{
-			try
-			{
-				this.repCourant = repToChange;
-				this.contenu.clear();
-				File[] listFiles = this.repCourant.listFiles();
-				for (int i = 0; i<listFiles.length; i++){
-					this.contenu.add(listFiles[i]);
-//					System.out.println("FileListing.setRepCourant() "+contenu.get(i));
-				}
-			}
-			catch(Exception e)
-			{
-				System.out.println("Fichier/Dossier inexistant");
-			}
+		this.repCourant = repToChange;
+		System.out.println("FileListing.setRepCourant() repCourant après setter : "+ this.repCourant);
+		this.contenu.clear();
+		this.fileNames.clear();
+		File[] listFiles = this.repCourant.listFiles();
+//		System.out.println("FileListing.setRepCourant() listfile.length() : "+ listFiles.length);
+		for (int i = 0; i < listFiles.length; i++) {
+			this.contenu.add(listFiles[i]);
+			this.fileNames.add(listFiles[i].getName());
 		}
-		else
-			System.out.println("FileListing.setRepCourant() pas un repertoire");
 	}
 
 	public List<File> getContenu() {
 		return this.contenu;
 	}
-	
-	/////////////////////////////////////////////////////////////////////
-	
+
+	// ///////////////////////////////////////////////////////////////////
+
 	/**
 	 * Remonter dans l'arborescence
+	 * 
 	 * @param null
-	 * @return 
+	 * @return
 	 */
-	public void remonter(){
+	public void remonter() {
 		try {
-			if (repCourant.getCanonicalPath().endsWith(":"))
-			{
-				System.out.println("FileListing.remonter() impossible de remonter plus, repertoire courant = root");
-			}
-			else
-			{
+			if (repCourant.getCanonicalPath().endsWith(":")) {
+				System.out
+						.println("FileListing.remonter() impossible de remonter plus, repertoire courant = root");
+			} else {
 				String currentPath = repCourant.getCanonicalPath();
-				String [] folders = currentPath.split("\\\\");
-				String upPath ="";
-				for (int i=0; i<folders.length-1; i++)
-				{
-					if(i==folders.length-2)
+				String[] folders = currentPath.split("\\\\");
+				String upPath = "";
+				for (int i = 0; i < folders.length - 1; i++) {
+					if (i == folders.length - 2)
 						upPath += folders[i];
 					else
-						upPath += folders[i]+"\\";
+						upPath += folders[i] + "\\";
 				}
-				System.out.println("------------------------------- \n FileListing.remonter() "+upPath);
+				System.out
+						.println("------------------------------- \n FileListing.remonter() "
+								+ upPath);
 				setRepCourant(upPath);
-				System.out.println("FileListing.remonter() après le setRepCourant : "+repCourant.getCanonicalPath());
+				System.out
+						.println("FileListing.remonter() après le setRepCourant : "
+								+ repCourant.getCanonicalPath());
 				File[] listFiles = repCourant.listFiles();
 				contenu.clear();
-				for (int i = 0; i<listFiles.length; i++){
-//					System.out.println("FileListing.remonter() "+listFiles[i]);
+				fileNames.clear();
+				for (int i = 0; i < listFiles.length; i++) {
+					// System.out.println("FileListing.remonter() "+listFiles[i]);
 					contenu.add(listFiles[i]);
+					fileNames.add(listFiles[i].getName());
 				}
 			}
 		} catch (IOException e) {
@@ -111,71 +110,42 @@ public class FileListing {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Créer un dossier/fichier, dans le dossier courant
-	 * @param nomDossier/fichier
+	 * 
+	 * @param nomDossier
+	 *            /fichier
 	 * @return boolean création possible ou impossible
 	 */
-	public boolean createFile(String name){
+	public boolean createFile(String name) {
 		boolean result = new File(name).mkdir();
 		return result;
 	}
-	
 
-	
-	public String toString(){
+	public String toString() {
 		String res = "";
-		for (int i = 0; i<contenu.size(); i++)
-		{
-			res +=contenu+"\n";
+		for (int i = 0; i < contenu.size(); i++) {
+			res += contenu + "\n";
 		}
 		return res;
 	}
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		FileListing list = null;
 		list = new FileListing();
+
+		list.setRepCourant("C:\\Users\\deptinfo\\Documents\\Ma musique");
 		
-		for (int i = 0; i<list.contenu.size(); i++){
+		for (int i = 0; i < list.contenu.size(); i++) {
 			try {
-				System.out.println("FileListing.main() "+list.contenu.get(i).getCanonicalPath());
+				System.out.println("FileListing.main() "
+						+ list.contenu.get(i).getCanonicalPath());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-//		File c = new File("C:");
-//		System.out.println("c : "+c.getAbsolutePath());
-//		
-//		list.setRepCourant("C:");
-		
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		list.remonter();
-//		System.out.println(list.repCourant.getCanonicalPath());
-//		
-//		list.setRepCourant("C:\\Users\\deptinfo\\Documents");
-//		list.remonter();
-//		list.remonter();
-		
-//		list.remonter();
-//		list.remonter();
-//		list.createFile("Test");
-		
-//		list.toString();
-		
 	}
-	
+
 }
--- src/fr/miage/fileListing/GUI.java
+++ src/fr/miage/fileListing/GUI.java
@@ -8,24 +8,38 @@ import javax.swing.JButton;
 import java.awt.TextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JMenuBar;
 import javax.swing.JScrollBar;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTree;
+import javax.swing.ListModel;
+import javax.swing.JScrollPane;
 
 public class GUI extends JFrame {
 
-	FileListing list = new FileListing();
+	FileListing listing;
 	Model model = new Model();
-	
-	GUI()
-	{
+	JList list;
+	JScrollPane scrollPane;
+
+	GUI() {
+		listing = new FileListing();
+		model.setRepCourant(listing.getRepCourant());
+		model.setContenu(listing.getContenu());
+		model.setFileNames(listing.getFileNames());
+
 		this.setTitle("Explorateur");
 		this.setSize(500, 500);
 		getContentPane().setLayout(null);
-		
+
 		// créer un nouveau dossier, dans le repertoire courant
 		JButton newFile = new JButton("New");
 		newFile.addActionListener(new ActionListener() {
@@ -33,59 +47,169 @@ public class GUI extends JFrame {
 				CreateFolderGUI createFolder = new CreateFolderGUI();
 			}
 		});
-		newFile.setBounds(129, 28, 53, 23);
+
+		newFile.setBounds(117, 28, 67, 23);
 		getContentPane().add(newFile);
-		
+
 		// remonter l'arborescence d'un cran
-		JButton btnRemonter = new JButton("Remonter");
+		JButton btnRemonter = new JButton("UP");
+		btnRemonter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				listing.remonter();
+				rebuildList();
+		}});
+
 		btnRemonter.setBounds(208, 28, 79, 23);
 		getContentPane().add(btnRemonter);
-		
-		
-		// liste du contenu du dossier
-		JList list = new JList();
-//		list.add(model.getContenu());
-		list.setBounds(155, 76, 300, 205);
-		getContentPane().add(list);
-		
+
 		// retourner à c: ou au bureau, à définir
 		JButton btnHome = new JButton("Home");
-		btnHome.setBounds(45, 28, 59, 23);
+		btnHome.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				listing.setRepCourant("C:\\Users\\deptinfo\\Documents\\TP\\Prog_Projet");
+				rebuildList();
+			}
+		});
+
+		btnHome.setBounds(21, 28, 74, 23);
 		getContentPane().add(btnHome);
+
+		scrollPane = new JScrollPane();
+		scrollPane.setSize(305, 205);
+		scrollPane.setLocation(155, 75);
+		list = new JList(Model.getFileNames().toArray());
+		list.setBounds(155, 76, 319, 205);
 		
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(457, 76, 17, 205);
-		getContentPane().add(scrollBar);
-		
-		
+		GUI.this.list.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent evt) {
+				JList list = (JList) evt.getSource();
+				if (evt.getClickCount() == 2) {
+					int index = list.locationToIndex(evt.getPoint());
+					if (GUI.this.model.getContenu(index).isDirectory()) {
+						try {
+							
+							listing.setRepCourant(GUI.this.model.getContenu(index).getCanonicalPath());
+							rebuildList();
+							
+							
+							
+							
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					else
+					{
+						System.out.println("GUI.rebuildList().new MouseAdapter() {...}.mouseClicked() pas un repertoire!");
+					}
+				} else if (evt.getClickCount() == 3) { // Triple-click
+					int index = list.locationToIndex(evt.getPoint());
+
+				}
+			}
+
+		});
+
+		scrollPane.setViewportView(list);
+		getContentPane().add(scrollPane);
+
 		JLabel lblPlugins = new JLabel("Plugins de visualisation");
 		lblPlugins.setBounds(21, 323, 115, 14);
 		getContentPane().add(lblPlugins);
-		
+
 		JLabel lblPluginsDanalyse = new JLabel("Plugins d'analyse");
 		lblPluginsDanalyse.setBounds(21, 374, 115, 14);
 		getContentPane().add(lblPluginsDanalyse);
-		
-		// liste des plugins de visualisation disponible, qu'on chargera à l'aide d'un classLoader personnalisé
+
+		// liste des plugins de visualisation disponible, qu'on chargera à
+		// l'aide d'un classLoader personnalisé
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(245, 320, 137, 20);
 		getContentPane().add(comboBox);
-		
-		// liste des plugins d'analyse disponible, qu'on chargera à l'aide d'un classLoader personnalisé
+
+		// liste des plugins d'analyse disponible, qu'on chargera à l'aide d'un
+		// classLoader personnalisé
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(245, 371, 137, 20);
 		getContentPane().add(comboBox_1);
-		
+
 		JTree tree = new JTree();
 		tree.setBounds(21, 76, 124, 205);
 		getContentPane().add(tree);
-		
+
+		// JScrollPane scrollPane = new JScrollPane();
+		// scrollPane.setBounds(155, 76, 200, 50);
+		// getContentPane().add(scrollPane);
+		// this.pack();
 		this.setVisible(true);
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
+	
+	public void rebuildList(){
+		Model.setRepCourant(listing.getRepCourant());
+		Model.setContenu(listing.getContenu());
+		Model.setFileNames(listing.getFileNames());
+
+		// refreshing
+
+		GUI.this.getContentPane().remove(GUI.this.list);
+		GUI.this.getContentPane().remove(GUI.this.scrollPane);
+
+		GUI.this.scrollPane.removeAll();
+		GUI.this.list.removeAll();
+
+		GUI.this.list.clearSelection();
+
+		GUI.this.scrollPane = new JScrollPane();
+		GUI.this.scrollPane.setSize(305, 205);
+		GUI.this.scrollPane.setLocation(155, 75);
+
+		GUI.this.list = new JList(Model.getFileNames().toArray());
+		GUI.this.list.setBounds(155, 76, 319, 205);
+
+		GUI.this.list.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent evt) {
+				JList list = (JList) evt.getSource();
+				if (evt.getClickCount() == 2) {
+					int index = list.locationToIndex(evt.getPoint());
+					if (GUI.this.model.getContenu(index).isDirectory()) {
+						try {
+							
+							listing.setRepCourant(GUI.this.model.getContenu(index).getCanonicalPath());
+							rebuildList();
+							
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					else
+					{
+						System.out.println("GUI.rebuildList().new MouseAdapter() {...}.mouseClicked() pas un repertoire!");
+					}
+					
+				} else if (evt.getClickCount() == 3) { // Triple-click
+					int index = list.locationToIndex(evt.getPoint());
+
+				}
+			}
+
+		});
+
+		GUI.this.scrollPane.setViewportView(GUI.this.list);
+		GUI.this.getContentPane().add(scrollPane);
+		GUI.this.getContentPane().revalidate();
+		GUI.this.getContentPane().repaint();
+
+	}
+
 	
 	public static void main(String[] args) {
 		GUI myGUI = new GUI();
-		
+
 	}
 }
